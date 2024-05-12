4c4d68ef4c487823162a3cf81e624691718ffb5d
YaroKl <wolfenstein169@gmail.com>
2024-05-12 19:40:41 +0300

    Added News class and ConsoleWriteLine logic

--- Polymorphism_Example/Program.cs
+++ Polymorphism_Example/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Polymorphism_Example
+﻿using System.ComponentModel;
+
+namespace Polymorphism_Example
 {
     internal class Program
     {
+        public class News
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DateTime Created { get; set; } = DateTime.Now;
+            public string GetHappyNews()
+            {
+                string happyNews = "Happy!\n " + Description;
+                return happyNews;
+            }
+        }
+        
         static void Main(string[] args)
         {
+            News newsObject = new News();
+            newsObject.Description = "First News";
+
+
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(newsObject.Description);
+
+            Console.WriteLine(newsObject.GetHappyNews());
         }
     }
 }
